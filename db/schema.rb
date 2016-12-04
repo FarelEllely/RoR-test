7b6da150394452eaf266500e28ba974ebd8b3a52
FarelEllely <Edy.ellely@gmail.com>
2016-12-04 19:35:30 +0530

    Add Message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204131103) do
+ActiveRecord::Schema.define(version: 20161204140422) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -113,6 +113,16 @@ ActiveRecord::Schema.define(version: 20161204131103) do
     t.string   "message_id"
     t.index ["notification_id"], name: "index_mailboxer_receipts_on_notification_id"
     t.index ["receiver_id", "receiver_type"], name: "index_mailboxer_receipts_on_receiver_id_and_receiver_type"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "conversation_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
