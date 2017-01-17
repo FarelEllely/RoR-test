b62029543cb81aef3c703fdd11f8cfc3431ed852
FarelEllely <Edy.ellely@gmail.com>
2017-01-17 16:57:06 +0530

    Set up Google SMTP for testing purpose

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_attached_file :image, styles: { medium: "350x350>", thumb: "100x100>", tiny: "x80" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
