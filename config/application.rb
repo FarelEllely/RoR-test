911d316a96f93e9da1671a974f5f826365a5f8d0
FarelEllely <Edy.ellely@gmail.com>
2017-01-30 05:56:45 +0530

    layout

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Echo
       :address  => 'smtp.gmail.com',
       :domain   => 'mail.google.com',
       :port     => 587,
-      :user_name=> 'ellelyf@gmail.com',
+      :user_name=> 'contact@tefecho.com',
       :password => 'ruthellely',
       :authentication => 'plain',
       :enable_starttls_auto => true
